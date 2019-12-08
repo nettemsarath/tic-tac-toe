3299f8059e4a9b4c899a4ee33f518b0c0f94abc2
nettem sarath <nettem@nvestbank.com>
2019-12-08 14:58:41 +0530

    fixing bugs

--- src/component/sarath.js
+++ src/component/sarath.js
@@ -13,26 +13,46 @@ class Sarath extends Component{
             matched:false
         }
 
+
+    }
+    player_next =()=>{
+        console.log('Handling Player next...')
+        let newarray = this.state.Array ;
+        let p= true ;
+        while(p){
+            let i = Math.floor(Math.random() * 3) ;
+            let j= Math.floor(Math.random() * 3) ;
+            if(newarray[i][j]===''){
+                newarray[i][j]='O'
+                this.setState({
+                    Array: newarray
+                })
+                p=false
+                console.log('New Array is :', newarray)
+            }
+        }
+        this.didhewon()
     }
     didhewon = async()=>{
-        let NLR =await this.state.Array 
+        console.log('Handling Did he won...')
+        let NLR = this.state.Array 
         let i,j ;
         for( i=0;i<3;i++){
 	    j=0;
 		if((NLR[i][j]===NLR[i][j+1]) && (NLR[i][j]===NLR[i][j+2])){
             //console.log("hiii 1111")
-            let match = { won:NLR[i][j] , success:true}
-            await this.setState({
+            let match = { won:NLR[i][j] , success:true} ;
+            console.log(match)
+           await this.setState({
                 match:match
             })
-            console.log(this.state.match)
-
+           
 		}
 		if((NLR[j][i]===NLR[j+1][i]) && (NLR[j][i]===NLR[j+2][i])){
             //console.log("hiii 222")
             let match = { won:NLR[j][i] , success:true}
             
-            await this.setState({
+           await this.setState({
                 match:match
             })
             console.log(this.state.match)
@@ -42,7 +62,7 @@ class Sarath extends Component{
             //console.log("diagonal")
             let match = { won:NLR[j][j], success:true}
             
-            await this.setState({
+           await this.setState({
                 match:match
             })
            console.log(this.state.match)
@@ -53,52 +73,29 @@ class Sarath extends Component{
         if( (NLR[2][0]===NLR[1][1]) &&(NLR[2][0]===NLR[0][2]) ){
         let match = { won:NLR[2][0], success:true}
         
-        await this.setState({
+       await this.setState({
             match:match
         })
-        }
-        
-            i = Math.floor(Math.random() * 3) ;
-            j= Math.floor(Math.random() * 3) ;
-            let newarray = this.state.Array ;
-            let p= true ;
-        while(p){
-            let i = Math.floor(Math.random() * 3) ;
-            let j= Math.floor(Math.random() * 3) ;
-            if(newarray[i][j]===''){
-                newarray[i][j]='O'
-                this.setState({
-                    Array: newarray
-                })
-                p=false
-            }
-        }
-        
-        //setInterval(Timefunction(this), 3000)
 
-        console.log(this.state.match)
+        }
+        return 1 ;
 
     }
-    handleChange = async(i,j)=>{
-        if(this.state.match.won===null || this.state.match.won===''){
+
+    handleChange = async (i,j)=>{
+        console.log('Handling Change..')
+        if( this.state.match.won===null || this.state.match.won==='' ){
             console.log("hiiiiiii")
             console.log(i,j);
             let newarray = [...this.state.Array ];
-    
-           // this.state.next_player?newarray[i][j]='X':newarray[i][j]='O'
-           newarray[i][j]='X'
+            newarray[i][j]='X'
             await this.setState({
                 Array:newarray
-            })
-            /*
-           await this.setState( previousstate =>{
-               return {
-                next_player : !previousstate.next_player 
-               }
-           })
-           */
-           // console.log(this.state.Array)
-            this.didhewon()
+            }) 
+            console.log( this.state.Array )
+           await this.didhewon() ;
+           this.player_next() ;
+
         }
     }
     render(){
